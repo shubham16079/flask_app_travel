4d22d2fa11fef0a21d4c169702c0561905c54e33
Shubham Singh <Shubham@Vcarenoida.com>
2024-01-18 22:41:08 +0530

    home page changes

--- app/routes.py
+++ app/routes.py
@@ -32,7 +32,10 @@ def inject_data():
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    page = request.args.get('page', 1, type=int)
+    per_page = 8
+    home_destinations = models.Destination.query.paginate(page=page, per_page=per_page, error_out=False)
+    return render_template('home.html', home_destinations=home_destinations)
 
 
 @app.route('/set-locale/<lang>')
